756e9d1e05f06317b4d003b348f2e573a82ac2ee
ClementBarrier <clement.barrier@epitech.eu>
2023-08-28 19:10:16 +0200

    [ADD] types

---
+++ app/src/types/ChickenData.tsx
@@ -0,0 +1,10 @@
+interface ChickenData {
+    id: number;
+    name: string;
+    birthday: string;
+    weight: string;
+    steps: number;
+    isrunning: boolean;
+}
+
+export default ChickenData;
